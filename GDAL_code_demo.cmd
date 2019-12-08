455292c4e101ae9acd94071f5ee1aa121334efaa
a-knudsen <52292767+a-knudsen@users.noreply.github.com>
2019-12-08 18:03:08 -0500

    Update GDAL_code_demo.cmd

--- GDAL_code_demo.cmd
+++ GDAL_code_demo.cmd
@@ -1,25 +1,26 @@
 # Ahna Knudsen 
 # GDAL Demo 
 # December 5, 2019
-# Check installation, move raster file to geopackage, and merge shapefiles
+# Description: Check installation, move raster file to geopackage, and merge shapefiles
+# Code was written in Terminal. Make sure to set directory. 
 
-# did not work due to path error 
-Ahnas-MacBook-Pro:~ ahnaknudsen$ ogr2ogr
+# functions to test if GDAL is downloaded, however, did not work due to path error 
+ogr2ogr
 -bash: ogr2ogr: command not found
-Ahnas-MacBook-Pro:~ ahnaknudsen$ brew install gdal2
+brew install gdal2
 -bash: brew: command not found
-Ahnas-MacBook-Pro:~ ahnaknudsen$ gdalinfo --version
+gdalinfo --version
 -bash: gdalinfo: command not found
 # fixed path error with echo  
-Ahnas-MacBook-Pro:~ ahnaknudsen$ echo 'export PATH=/Library/Frameworks/GDAL.framework/Programs:$PATH' >> ~/.bash_profile
-Ahnas-MacBook-Pro:~ ahnaknudsen$ source ~/.bash_profile
+echo 'export PATH=/Library/Frameworks/GDAL.framework/Programs:$PATH' >> ~/.bash_profile
+source ~/.bash_profile
 # check to make sure installed 
-Ahnas-MacBook-Pro:~ ahnaknudsen$ gdalinfo --version
+gdalinfo --version
 GDAL 2.4.2, released 2019/06/28
 
 
-# check if gdal is working 
-Ahnas-MacBook-Pro:NE2_LR_LC_SR_W ahnaknudsen$ gdal_translate
+# another check to see if gdal is working 
+gdal_translate
 Usage: gdal_translate [--help-general] [--long-usage]
        [-ot {Byte/Int16/UInt16/UInt32/Int32/Float32/Float64/
              CInt16/CInt32/CFloat32/CFloat64}] [-strict]
@@ -41,20 +42,22 @@ Usage: gdal_translate [--help-general] [--long-usage]
 
 FAILURE: No source dataset specified.
 
+
+# Functions for Demo:
 # Move raster file to geopackage 
-# raster file: NE2_LR_LC_SR_W.tif
+# Raster file: NE2_LR_LC_SR_W.tif
 # Geopackage: myNewGPKG.gpkg
 # of = output format and co = creation option
 # QUALITY = 100 means there will be no compression
 
-Ahnas-MacBook-Pro:NE2_LR_LC_SR_W ahnaknudsen$ gdal_translate -of GPKG NE2_LR_LC_SR_W.tif myNewGPKG.gpkg -co QUALITY=100
+gdal_translate -of GPKG NE2_LR_LC_SR_W.tif myNewGPKG.gpkg -co QUALITY=100
 Input file size is 16200, 8100
 0...10...20...30...40...50...60...70...80...90...100 - done.
 
 # Repeat with additional parameters 
 # Creation option - append subdataset to existing geopackage
 # Creation options: Raster_Table to remane raster table
-Ahnas-MacBook-Pro:NE2_LR_LC_SR_W ahnaknudsen$ gdal_translate -of GPKG NE2_LR_LC_SR_W.tif myNewGPKG.gpkg -co QUALITY=100 -co APPEND_SUBDATASET=YES -co RASTER_TABLE=elevTable
+gdal_translate -of GPKG NE2_LR_LC_SR_W.tif myNewGPKG.gpkg -co QUALITY=100 -co APPEND_SUBDATASET=YES -co RASTER_TABLE=elevTable
 Input file size is 16200, 8100
 0...10...20...30...40...50...60...70...80...90...100 - done.
 
@@ -69,11 +72,11 @@ ogr2ogr -f ‘ESRI Shapefile’ -update -append merge.shp filename2.shp -nln mer
 # create new shape file called “merge”
 # then update and merge shapefiles into it
 # shapefile names: study_area_boundary.shp and Counties_StudyArea.shp (polygons)
-Ahnas-MacBook-Pro:Preprocessed_Data ahnaknudsen$ ogr2ogr -f 'ESRI Shapefile' merge.shp study_area_boundary.shp
-Ahnas-MacBook-Pro:Preprocessed_Data ahnaknudsen$ ogr2ogr -f 'ESRI Shapefile' -update -append merge.shp Counties_StudyArea.shp -nln merge
+ogr2ogr -f 'ESRI Shapefile' merge.shp study_area_boundary.shp
+ogr2ogr -f 'ESRI Shapefile' -update -append merge.shp Counties_StudyArea.shp -nln merge
 
 # repeat
 # shapefile names: GreatWall_Beijing.shp and Powerlines_Beijing.shp (lines)
-Ahnas-MacBook-Pro:Preprocessed_Data ahnaknudsen$ ogr2ogr -f 'ESRI Shapefile' mergePowerWall.shp GreatWall_Beijing.shp
-Ahnas-MacBook-Pro:Preprocessed_Data ahnaknudsen$ ogr2ogr -f 'ESRI Shapefile' -update -append mergePowerWall.shp Powerlines_Beijing.shp -nln merge
+ogr2ogr -f 'ESRI Shapefile' mergePowerWall.shp GreatWall_Beijing.shp
+ogr2ogr -f 'ESRI Shapefile' -update -append mergePowerWall.shp Powerlines_Beijing.shp -nln merge
 
